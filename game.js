d10dbac7ab1978ad5ef35e948ced5899ca7cabea
Evan <evanmarkert@gmail.com>
2022-03-22 20:36:00 -0400

    adding query selectors for selection buttons

--- game.js
+++ game.js
@@ -61,3 +61,7 @@ function game() {
         return "Computer wins. Better luck next time!"
     }
 };
+
+const rockButton = document.querySelector('#rock');
+const scissorsButton = document.querySelector('#scissors');
+const paperButton = document.querySelector('#paper');
